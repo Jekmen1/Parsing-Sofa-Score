341d61c4ec084bd33e6d564a53ce2f939a068cbf
Jekmen1 <151436749+Jekmen1@users.noreply.github.com>
2024-09-25 17:47:20 +0400

    Update livestatistics.py

--- livestatistics.py
+++ livestatistics.py
@@ -7,17 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# URL for the match details page
-# url = "https://www.sofascore.com/football/match/fortaleza-corinthians/hOsvP#id:12378088"
-# #
-# # # Get the HTML page and status code
-# response = requests.get(url)
-# print("HTML Page Status Code:", response.status_code)
-#
-# # Parse the HTML
-# soup = BeautifulSoup(response.text, "html.parser")
 
-# Update headers to match expected API request structure
 import requests
 
 headers = {
@@ -31,18 +21,17 @@ headers = {
     'referer': 'https://www.sofascore.com/',
     'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
 }
 
-# Make a request to the API to get the match statistics
+
 statistic_url = 'https://api.sofascore.com/api/v1/event/12065790/'
 response_statistics = requests.get(statistic_url, headers=headers)
 print("Statistics API Status Code:", response_statistics.status_code)
 
-# Parse the JSON response
+
 if response_statistics.status_code == 200:
     stats = response_statistics.json()
 
@@ -51,10 +40,8 @@ if response_statistics.status_code == 200:
         # Ask the user which statistic they want
         requested_stat = input("Enter the name of the statistic (e.g., Ball possession, Total shots, Fouls): ").strip()
 
-        # Flag to check if the stat was found
         stat_found = False
 
-        # Loop through the statistics in the JSON response
         for group in stats['statistics'][0]['groups']:
             for item in group['statisticsItems']:
                 if item['name'].lower() == requested_stat.lower():
